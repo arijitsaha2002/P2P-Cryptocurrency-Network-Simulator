87b7d7bc831a2c866dea28a26d99f7e7fde543f3
23-ryan <aryanmathe111@gmail.com>
2024-01-30 21:09:46 +0530

    Added Node implementations

--- node.cpp
+++ node.cpp
@@ -1,11 +1,52 @@
 #include "node.h"
+extern long double CURRENT_TIME;
 
-void Node::add_node_to_blockchain(Block* blk){
-       if(blk->get_length_of_chain() > longestChainTail->get_length_of_chain()){
-        longestChainTail = blk;
-       }
+Node::Node(){
+	this->node_id = node_id;
+}
+
+bool Node::add_block_to_tree(Block* blk){
+	Block* prev_blk = blk->prev_blk;
+
+	if(prev_blk->users_recv_time[node_id] == -1){
+		if(ukn_blocks.find(prev_blk->blk_id) == ukn_blocks.end()){
+			ukn_blocks[prev_blk->blk_id] = vector<Block*>({blk});
+		}
+		else{
+			ukn_blocks[prev_blk->blk_id].append(blk);
+		}
+
+		int curr_id = blk->blk_id;
+		while(ukn_blocks.find(curr_id) != ukn_blocks.end()){
+			Block* blk_child = ukn_blocks.find(cur_id)->second;
+			add_block_to_tree(blk_child);
+		}
+	
+		if(blk->get_length_of_chain() > longest_chain_tail->get_length_of_chain()){
+			longest_chain_tail = blk;
+		}
+		return true;
+	}
+
+	else return false;
 }
 
 void Node::add_transaction(Transaction* txn){
-    trasactionPool.insert(txn);
+	// pending
+}
+
+vector<Node*> Node::get_neighbours(){
+	return connected_peers;
+}
+
+Block* get_longest_chain_tail(){
+	return longest_chain_tail;
+}
+
+void Node::add_node(Node* neighbour){
+	connected_peers.push_back(neighbour);
+}
+
+bool populate_block(Block* blk){
+	// pending implmentation
 }
--- node.h
+++ node.h
@@ -1,17 +1,25 @@
+#ifndef NODE_H
+#define NODE_H
+
 #include "block.h"
 #include "utils.h"
 
 class Node {
     int node_id;
-    vector<Node*> connectedPeers;
-    set<Transaction*> trasactionPool;
-    Block* longestChainTail;
-
+    vector<Node*> connected_peers;
+    set<Transaction*> trasaction_pool;
+    Block* longest_chain_tail;
+	unordered_map<int, vector<Block*>> ukn_blocks;
+	
 public:
-    Node(int node_id){
-        this->node_id = node_id;
-    }
-
-    void add_transaction(Transaction* txn);
-    void add_node_to_blockchain(Block* blk);
+    Node(int node_id);
+    bool add_transaction(Transaction* txn); //
+    void add_block_to_tree(Block* blk);
+	vector<Node*> get_neighbours();
+	void add_node(Node* neighbour);
+	void populate_block(Block* blk); //
+	Block* get_longest_chain_tail();
 };
+
+
+#endif
