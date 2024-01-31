771db8329460441926026a16d8762658df852c33
Khushang Singla <khushangsingla@gmail.com>
2024-01-31 17:27:12 +0530

    Added docstrings

--- events.h
+++ events.h
@@ -3,6 +3,20 @@
 #include "block.h"
 #include "transaction.h"
 
+/**
+ * @file events.h
+ * @brief Contains the class definitions for various events
+ *
+ * Contains the class definitions
+ */
+
+/**
+ * Enum for event types
+ * EVENT_TRANSACTION_GENERATE: Event leading to a new transaction being generated
+ * EVENT_TRANSACTION_RECIEVE: Event leading to a transaction being recieved by a node
+ * EVENT_BLOCK_GENERATE: Event leading to a new block being generated
+ * EVENT_BLOCK_RECIEVE: Event leading to a block being recieved by a node
+ */
 enum event_type
 {
 	EVENT_TRANSACTION_GENERATE,
@@ -11,6 +25,11 @@ enum event_type
 	EVENT_BLOCK_RECIEVE
 };
 
+/**
+ * @class Event
+ *
+ * Class for events
+ */
 class Event
 {
 	long double timestamp;
@@ -18,11 +37,27 @@ class Event
 
 	public:
 
+	/**
+	 * @brief Constructor for Event
+	 *
+	 * @param time The time at which the event occurs
+	 * @param type The type of the event
+	 */
 	Event(long double time, int type);
 	bool operator<(const Event& rhs) const;
+	/**
+	 * @brief Function to simulate the event
+	 *
+	 * Pure virtual function to simulate the event
+	 */
 	virtual void simulate_event() = 0;
 };
 
+/**
+ * @class GenerateTransaction
+ *
+ * Class for event leading to a new transaction being generated
+ */
 class GenerateTransaction: public Event
 {
 	Node* creator_node;
@@ -30,9 +65,20 @@ class GenerateTransaction: public Event
 	void create_event_for_next_transaction();
 	public:
 
+	/**
+	 * @brief Constructor for GenerateTransaction
+	 *
+	 * @param time The time at which the event occurs
+	 * @param creator_node The node which creates the transaction
+	 */
 	GenerateTransaction(long double time, Node* creator_node);
 };
 
+/**
+ * @class TransactionRecieved
+ *
+ * Class for event leading to a transaction being recieved by a node
+ */
 class TransactionRecieved: public Event
 {
 	Transaction* transaction;
@@ -41,9 +87,22 @@ class TransactionRecieved: public Event
 
 	public:
 
+	/**
+	 * @brief Constructor for TransactionRecieved
+	 *
+	 * @param time The time at which the event occurs
+	 * @param transaction The transaction being recieved
+	 * @param reciever_node The node which recieves the transaction
+	 * @param sender_node The node which sends the transaction
+	 */
 	TransactionRecieved(long double time, Transaction* transaction, Node* reciever_node,Node* sender_node);
 };
 
+/**
+ * @class GenerateBlock
+ *
+ * Class for event leading to a new block being generated
+ */
 class GenerateBlock: public Event
 {
 	Node* creator_node;
@@ -53,10 +112,28 @@ class GenerateBlock: public Event
 
 	public:
 
+	/**
+	 * @brief Constructor for GenerateBlock
+	 *
+	 * @param time The time at which the event occurs
+	 * @param creator_node The node which creates the block
+	 * @param parent_block The parent block of the block being created
+	 */
 	GenerateBlock(long double time, Node* creator_node, Node* parent_block);
 };
 
+/**
+ * @brief Function to create an event for new block generation
+ *
+ * @param node The node which creates the block
+ */
 void create_event_for_next_block(Node* node);
+
+/**
+ * @class BlockRecieved
+ *
+ * Class for event leading to a block being recieved by a node
+ */
 class BlockRecieved: public Event
 {
 	Block* block;
@@ -65,6 +142,14 @@ class BlockRecieved: public Event
 
 	public:
 
+	/**
+	 * @brief Constructor for BlockRecieved
+	 *
+	 * @param time The time at which the event occurs
+	 * @param block The block being recieved
+	 * @param reciever_node The node which recieves the block
+	 * @param sender_node The node which sends the block
+	 */
 	BlockRecieved(long double time, Block* block, Node* reciever_node, Node* sender_node);
 };
 
