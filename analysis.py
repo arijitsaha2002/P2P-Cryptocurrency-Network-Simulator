cfd31fc525c193e80b5a41441a6c8aedca2cdfc4
Arijit <sontu1729@gmail.com>
2024-02-12 18:29:31 +0530

    make blockchain tree function done

--- analysis.py
+++ analysis.py
@@ -1,12 +1,26 @@
 #!/bin/python3
 import argparse
 import os
-
+import pandas as pd
+import networkx as nx
+from networkx.drawing.nx_agraph import graphviz_layout
+import matplotlib.pyplot as plt
 
 
 def make_blockchain_tree(filename):
-    pass
+    T = nx.Graph()
+    file = pd.read_csv(filename)
 
+    for a, b in zip(file.id, file.pid):
+        if( a < 150 and b < 150 and a >= 0 and b >= 0):
+            T.add_edge(b, a)
+
+    G = T 
+    pos = graphviz_layout(G, prog='dot', args='-Grankdir="LR"')
+    nx.draw_networkx_nodes(G, pos, node_size=300)
+    nx.draw_networkx_edges(G, pos, edgelist=G.edges(), arrows=True, arrowstyle="<-")
+    # nx.draw_networkx_labels(G, pos, font_size=7, font_color="white")
+    plt.show()
 
 if __name__ == "__main__":
     parse = argparse.ArgumentParser()
