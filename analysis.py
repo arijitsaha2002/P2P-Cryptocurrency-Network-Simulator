41863646b411e4ef41f876c10a5e5a28d7bd29a6
Arijit <sontu1729@gmail.com>
2024-02-17 16:32:36 +0530

    analysis.py changed

--- analysis.py
+++ analysis.py
@@ -18,17 +18,22 @@ def make_blockchain_tree(filename):
 
     G = T 
     title = get_title_from_filename(filename)
-    plt.figure(figsize=(30,10))
+    plt.figure(figsize=(20,10))
     pos = graphviz_layout(G, prog='dot', args='-Grankdir="LR"')
     nx.draw_networkx_nodes(G, pos, node_size=300)
     nx.draw_networkx_edges(G, pos, edgelist=G.edges(), arrows=True, arrowstyle="<-")
     plt.title(f"{title}")
 
-def get_title_from_filename(filename):
-    params = os.path.basename(filename)[:-len(".log.csv")].split("_")[2:]
-    return "\n".join(str({"num_peers": params[0], "mean_interarrival_block_time": params[1], "frac_low_cpu": params[2], "mean_interarrival_transaction_time": params[3], "frac_slow": params[4] }).split(','))
+# def get_title_from_filename(filename):
+    # params = os.path.basename(filename)[:-len(".log.csv")].split("_")[2:]
+    # return "\n".join(str({"num_peers": params[0], "mean_interarrival_block_time": params[1], "frac_low_cpu": params[2], "mean_interarrival_transaction_time": params[3], "frac_slow": params[4] }).split(','))
     
 
+def get_title_from_filename(filename):
+    params = os.path.basename(filename)[:-len(".log.csv")].split("_")[2:]
+    ret = f"({params[1]}, {params[2]}, {params[3]}, {params[4]})"
+    return ret;
+    
 
 
     
@@ -56,7 +61,7 @@ def full_analysis(dirname):
         make_blockchain_tree(os.path.join(dirname, r[0]))
         plt.savefig(os.path.join(os.path.dirname(os.path.join(dirname, r[0])), f'tree_{os.path.basename(os.path.join(dirname, r[0]))[len("block_info_"):-len(".log.csv")]}.png'))
         plt.close()
-        titles.append(str(title))
+        titles.append(title)
         longest_single_chain, total_single_chain = do_ratio_analysis(os.path.join(dirname,r[0]), os.path.join(dirname,r[1]))
         longest_chain.append(longest_single_chain)
         total_chain.append(total_single_chain)
@@ -67,7 +72,7 @@ def full_analysis(dirname):
       
     X_axis = numpy.arange(len(titles)) 
     longest_chain = numpy.array(longest_chain).T
-    plt.figure(figsize=(30,10))
+    plt.figure(figsize=(15,10))
     plt.bar(X_axis - 0.1, longest_chain[3], 0.1, label = 'LOW CPU-SLOW SPEED') 
     plt.bar(X_axis, longest_chain[2], 0.1, label = 'LOW CPU-FAST SPEED') 
     plt.bar(X_axis + 0.1, longest_chain[1], 0.1, label = 'HIGH CPU-SLOW SPEED') 
@@ -76,13 +81,14 @@ def full_analysis(dirname):
     plt.xticks(X_axis, titles) 
     plt.ylabel("Average ratio of contribution of the node type") 
     plt.title("Longest chain in blockchain") 
+    plt.xlabel(str({"num_peers", "mean_interarrival_block_time", "frac_low_cpu", "mean_interarrival_transaction_time", "frac_slow"}))
     plt.legend() 
     plt.savefig(os.path.join(dirname, f'longest_chain_contrib_comp.png'))
     plt.close()
 
     X_axis = numpy.arange(len(titles)) 
     total_chain = numpy.array(total_chain).T
-    plt.figure(figsize=(30,10))
+    plt.figure(figsize=(15,10))
     plt.bar(X_axis - 0.1, total_chain[3], 0.1, label = 'LOW CPU-SLOW SPEED') 
     plt.bar(X_axis, total_chain[2], 0.1, label = 'LOW CPU-FAST SPEED') 
     plt.bar(X_axis + 0.1, total_chain[1], 0.1, label = 'HIGH CPU-SLOW SPEED') 
@@ -90,7 +96,8 @@ def full_analysis(dirname):
       
     plt.xticks(X_axis, titles) 
     plt.ylabel("Average ratio of contribution of the node type") 
-    plt.title("Full chain in blockchain") 
+    plt.title("Full blockchain") 
+    plt.xlabel(str({"num_peers", "mean_interarrival_block_time", "frac_low_cpu", "mean_interarrival_transaction_time", "frac_slow"}))
     plt.legend() 
     plt.savefig(os.path.join(dirname, f'full_chain_contrib_comp.png'))
     plt.close()
